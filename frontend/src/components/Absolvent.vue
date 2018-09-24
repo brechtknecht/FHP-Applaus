435e6373cf2eaff06ac17b3a1ed0ba9d288f6372
Felix Tesche <felix.tesche@gmail.com>
2018-09-24 22:29:48 +0200

    real initial commit

---
+++ frontend/src/components/Absolvent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div :class="'absolvent-' + data.id">
+      <h5> {{ data.forename }} {{ data.lastname }} </h5>
+      <em class="absolvent-desc"> <abbr> {{ data.title }} ·</abbr> {{ data.undertitle }} </em>
+    </div>
+</template>
+
+<script>
+	export default {
+		name: 'Absolvent',
+		props: {
+			data: Object
+    },
+    created(){
+      // console.log(this.data);
+    }
+  }
+</script>
+
+<style lang="scss">
+  .absolvent-desc {
+    font-size: 1em;
+    line-height: 1.4em;
+  }
+</style>
---
+++ frontend/src/components/Kontakt.vue
@@ -0,0 +1,42 @@
+<template>
+	<footer>
+		<div class="section" v-for="section in content.body" :key="section.template">
+			<h4 v-if="section.name" v-html="section.name" ></h4>
+			<div class="text" v-html="section.content"></div>
+		</div>
+	</footer>
+</template>
+
+<script>
+	export default {
+		props: {
+			content: Object
+		}
+	}
+
+</script>
+
+<style lang="scss">
+	#kontakt footer {
+		display: flex;
+		direction: row;
+		flex-wrap: nowrap;
+		justify-content: space-between;
+		align-items: flex-start;
+
+		.section {
+			flex-basis: calc(20% - 2.272727273rem);
+							h4 {
+					text-transform: uppercase;
+					letter-spacing: .05em;
+				}
+			.text {
+				font-size: .875rem;
+				p {
+					margin-bottom: 1.5em;
+				}
+
+			}
+		}
+	}
+</style>
