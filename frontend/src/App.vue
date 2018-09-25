7c889425e9fc5a0611310e4f852141ff5aaf397b
Felix Tesche <felix.tesche@gmail.com>
2018-09-25 14:36:07 +0200

    Added Media-Queries File

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -24,7 +24,6 @@ export default {
 
 </script>
 <style lang="scss">
-
 	body {
 		text-rendering: optimizeLegibility;
 		-webkit-font-smoothing: antialiased;	
--- frontend/src/views/Aussteller.vue
+++ frontend/src/views/Aussteller.vue
@@ -46,6 +46,8 @@ export default {
 }
 </script>
 <style lang="scss">
+	@import '@/scss/mediaqueries.scss';
+
 	.body {
 		display: flex;
 		flex-direction: row;
