489b16c6f7ff18c664c40bd3345ca03bc896d2a1
Felix Tesche <felix.tesche@gmail.com>
2018-10-14 16:21:45 +0200

    Impressum really added

---
+++ frontend/src/views/Impressum.vue
@@ -0,0 +1,116 @@
+<template>
+	<div class="impressum">
+		<!-- <Header class="header" :content="this.$root.$options.config"></Header> -->
+		<!-- <Navigation :items="this.$root.$options.navigation" reactiveNav></Navigation> -->
+		<h3>Applaus 2018</h3>
+        <h2>IMPRESSUM</h2>
+
+        <h3>Hausanschrift</h3>
+        <p class="fatspace">
+            Kiepenheuerallee 5 <br>
+            14469 Potsdam
+        </p>
+
+        <h3>Postanschrift</h3>
+        <p class="fatspace">
+            Postfach 60 06 08 <br>
+            14406 Potsdam <br>
+            Telefon +49 331 580 00 <br>
+            Fax +49 331 580 1009
+        </p>
+
+        <p class="fatspace">
+            Die Fachhochschule Potsdam ist eine Körperschaft des Öffentlichen Rechts. Sie wird durch den Präsidenten Herrn Prof. Dr. Eckehard Binas vertreten.
+        </p>
+
+        <h3>Zuständige Aufsichtsbehörde</h3>
+        <p>Ministerium für Wissenschaft, Forschung und Kultur des Landes Brandenburg</p>
+        <p>Dortustr. 36<br>14467 Potsdam</p>
+        <p class="fatspace">  
+            <a href="http://www.mwfk.brandenburg.de" title="Website des Ministeriums für Wissenschaft, Forschung und Kultur des Landes Brandenburg" target="_blank">www.mwfk.brandenburg.de</a>
+        </p>
+
+        <h3>Umsatzsteuer-Identifikationsnummer</h3>
+        <p class="fatspace">
+            <em>(gemäß § 27a Umsatzsteuergesetz)</em><br>
+            DE 811920300
+        </p>
+
+        <h3>Verantwortliche Personen</h3> 
+        <p>Für die Medieninformationen im Sinne des Presserechtes verantwortlich:</p>
+        <p class="fatspace">
+            Birgit Lißke<br>
+            Tel. +49 331 580 1004
+        </p>
+
+        <h3> Gestaltung &amp; Entwicklung </h3>
+        <h5>Projektleitung</h5>
+        <p>Prof. Matthias Beyrow</p>
+
+        <h5>Gestaltung</h5>
+        <a href="http://knisterwerk.com" title="Website von Knisterwerk" target="_blank">Phillip Kortlang &amp; Chris Magiera</a>
+
+        <h5>Gestaltung</h5>
+        <p><a href="http://knisterwerk.com" title="Website von Knisterwerk" target="_blank">Chris Magiera</a>, <a href="http://about.incom.org" title="Website von Incom" target="_blank">Incom</a> &amp; Juri Wolf</p>
+
+        <h5>Fotografien</h5>
+        <p>Aussteller</p>
+
+        <h3> Haftungsausschluss </h3>
+        <p>Eine Haftung für die Richtigkeit, Vollständigkeit und Aktualität dieser Webseiten kann trotz sorgfältiger Prüfung nicht übernommen werden. Die Fachhochschule Potsdam übernimmt insbesondere keine Haftung für eventuelle Schäden oder Konsequenzen, die durch eine direkte oder indirekte Nutzung der angebotenen Inhalte entstehen.</p>
+        <p>Es wird ausdrücklich darauf hingewiesen, dass für den Inhalt verlinkter Seiten ausschließlich deren Betreiber verantwortlich ist. Trotz sorgfältiger inhaltlicher Kontrolle übernehmen wir keine Haftung für die Inhalte externer Links. Für Schäden, die aus der Nutzung oder Nichtnutzung dieser Informationen entstehen, haftet ausschließlich der Betreiber der Seite, auf die verwiesen wurde.</p>
+        <p class="fatspace">Sollten Sie Kenntnis von verlinkten Seiten mit rechtswidrigen Inhalten erlangen, bitten wir Sie, uns dies mitzuteilen.</p>
+
+        <h3> Urheberrecht </h3>
+        <p>Alle im Internetangebot der Fachhochschule Potsdam veröffentlichten Inhalte (Layout, Texte, Bilder, Grafiken usw.) unterliegen dem Urheberrecht. Jede vom Urheberrechtsgesetz nicht zugelassene Verwertung bedarf vorheriger ausdrücklicher Zustimmung. Dies gilt insbesondere für Vervielfältigung, Bearbeitung, Übersetzung, Einspeicherung, Verarbeitung bzw. Wiedergabe von Inhalten in Datenbanken oder anderen elektronischen Medien und Systemen. Fotokopien und Downloads von Webseiten für den privaten, wissenschaftlichen und nicht kommerziellen Gebrauch dürfen hergestellt werden.</p>
+        <p class="fatspace">Wir erlauben ausdrücklich und begrüßen das Zitieren unserer Dokumente und Webseiten sowie das Setzen von Links auf unsere Website.</p>
+
+        <h3> Datenschutzerklärung </h3>
+        <p>Die Fachhochschule Potsdam unterliegt den Bestimmungen des Brandenburgischen Datenschutzgesetzes sowie den einschlägigen gesetzlichen Grundlagen mit Bestimmungen zum Schutz des Rechtes auf informationelle Selbstbestimmung.</p>
+        <p>Zum Schutz Ihrer Rechte haben wir technische und organisatorische Maßnahmen getroffen, dass die Vorschriften über den Datenschutz sowohl innerhalb der Fachhochschule Potsdam als auch von externen Dienstleistern beachtet werden, wenn diese an diesem Angebot mitwirken.</p>
+        <p>Ihre Daten dürfen nur in dem Umfange verarbeitet werden, wie spezielle Gesetze dies zulassen oder Ihre Einwilligung vorliegt.</p>
+        <p>In Verbindung mit Ihrem Zugriff auf das Internetangebot der Fachhochschule Potsdam werden auf unseren Servern im Falle des erfolglosen Abrufens einer Datei vorübergehend Daten zur Feststellung von Betriebsstörungen gespeichert, die möglicherweise eine Identifizierung zulassen. Dies können sein: Name der abgerufenen Datei, Datum und Uhrzeit des Abrufs und die abrufende IP-Adresse.</p>
+        <p>Bei der Verwendung der angebotenen Mail-Links werden nur die von Ihnen eingegebenen Daten an den Empfänger der Mail weitergeleitet. In Logdateien können zur Sicherstellung des Betriebes sowie zur Gewährleistung der Datensicherheit die Verbindungsdaten gespeichert werden. Eine Auswertung dieser Daten erfolgt nicht.</p>
+        <p>Ausgenommen hiervon sind: Auswertungen bei Vorliegen des Verdachtes einer Straftat und Auswertungen bei Vorliegen eines begründeten Missbrauchsverdachtes. Daten in anonymisierter Form für statistische Zwecke und zur Optimierung des Internetangebotes.</p>
+        <p class="fatspace">Wir weisen Sie darauf hin, dass die Datenübertragung, insbesondere per E-Mail, über das Internet derzeit im Wesentlichen ungesichert erfolgt. Es ist nicht ausgeschlossen, dass übermittelte Daten von Unbefugten zur Kenntnis genommen und eventuell sogar verfälscht werden können.</p>
+
+
+	</div>
+</template>
+
+<script>
+import Navigation from '@/components/Nav.vue'
+import Header from '@/components/Header.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Navigation,
+    Header
+  }
+}
+</script>
+
+<style lang="scss">
+    .impressum {
+        margin-top: 4rem;
+        padding: 6rem 0;
+        text-align: center;
+        h2 {
+            margin-bottom: 3rem;
+        }
+        h3 {
+            margin-top: .25rem;
+            margin-bottom: 1.2rem;
+        }
+        p {
+            margin: 0 auto;
+            margin-bottom: .7rem;
+            max-width: 360px;
+        }
+
+        .fatspace {
+            margin-bottom: 4rem;
+        }
+    }
+</style>
